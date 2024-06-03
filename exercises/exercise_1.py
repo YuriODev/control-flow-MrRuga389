549dd8ea01d5d621c69322c4db24d844ae09855d
MrRuga389 <rugashane21@gmail.com>
2024-06-03 08:49:34 +0000

    sol

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,8 @@
-# Your solution to Exercise 1
-
+alex = int(input(" what is alex's age: "))
+tatyana = int(input(" what is tatyana's age: "))
+if alex>tatyana:
+    print("Alex is the eldest.")
+elif tatyana>alex:
+    print("Tatyana is the eldest.")
+else:
+    print("Alex and Tatyana are of the same age.")
