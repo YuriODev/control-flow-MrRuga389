b44b75b9993f1b5b2580fa94427928bbc9c3225c
MrRuga389 <rugashane21@gmail.com>
2024-06-12 12:52:47 +0000

    push

--- exercises/exercise_8.py
+++ exercises/exercise_8.py
@@ -1,2 +1,11 @@
-# Your solution to Exercise 8
-
+a = int((input("")))
+b = int(input(""))
+c = "False"
+if b == a//100:
+    c = "True"
+elif b == (a//10)%10:
+    c = "True"
+elif b == a%10:
+    c = "True"
+print(c)
+    
