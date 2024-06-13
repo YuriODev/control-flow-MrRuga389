77e36da8064b85ff36ee652465c8561ba247593d
MrRuga389 <rugashane21@gmail.com>
2024-06-13 10:08:13 +0000

    bleu

--- exercises/exercise_6.py
+++ exercises/exercise_6.py
@@ -1,2 +1,10 @@
-# Your solution to Exercise 6
+x2, y2 = int(input("Enter x2: ")), int(input("Enter y2: "))
+da = x1**2 + y1**2
 
+db = x2**2 + y2**2
+if da > db:
+    print("A is further from the origin.")
+elif da < db:
+    print("B is further from the origin.")
+else:
+    print("A and B are at the same distance from the origin.")
